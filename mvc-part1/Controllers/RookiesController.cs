64ebf17969f4477f99a7bba84bceb093b39c9b1d
LamTran93 <lamtdth2208062@fpt.edu.vn>
2025-03-31 15:29:31 +0700

    update

---
+++ mvc-part1/Controllers/RookiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_part1.Models;
+
+namespace mvc_part1.Controllers
+{
+    public class RookiesController : Controller
+    {
+        private readonly List<PersonModel> _persons;
+
+        public RookiesController(List<PersonModel> persons)
+        {
+            _persons = persons;
+        }
+
+        public IActionResult Male()
+        {
+            return View(_persons);
+        }
+    }
+}
---
+++ mvc-part1/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿namespace mvc_part1.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public GenderType Gender { get; set; }
+        public DateOnly Birthday { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BirthPlace { get; set; }
+        public bool IsGraduated { get; set; }
+        
+        public enum GenderType
+        {
+            Male,
+            Female
+        }
+    }
+}
